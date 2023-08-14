eb32d4eade57f880d26efc81482b6fd47ce6bfe8
Igor.Nascimento <igor.nascimento.chan@liferay.com>
2023-08-14 11:00:48 -0300

    IMplementação do Meta Data.

--- hello.py
+++ hello.py
@@ -1,4 +1,24 @@
 #!/bin/env python3
+"""Hello World Multi Linguas.
+
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem 
+correspondente.
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+
+Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Bruno Rocha"
+__license__ = "Unlicense"
 
 
 import os
