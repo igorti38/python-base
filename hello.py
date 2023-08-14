37abdb9d3091edbae0995a0b938012075f4bcb85
Igor.Nascimento <igor.nascimento.chan@liferay.com>
2023-08-14 10:50:18 -0300

    )implamentação da variavel LANG e funcoes.

--- hello.py
+++ hello.py
@@ -1,4 +1,17 @@
 #!/bin/env python3
 
-print("Hello, World!")
+
+import os
+
+current_language = os.getenv("LANG", "en_US")
+
+msg = "Hello World"
+
+if current_language == "pt_BR" :
+    msg = "Ola, Mundo!"
+
+elif current_language == "it_IT" :
+   msg = "Ciao, Mondo!"    
+
+print(msg)
 
